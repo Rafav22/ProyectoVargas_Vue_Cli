f1009e3cddede78bb7bfa9ad226be80301e97ff8
Rafa <rafav22@gmail.com>
2023-06-12 20:23:12 -0600

    Proyecto Vue pasar CDN a CLI
    Proyecto Vue pasar CDN a CLI

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h1>Tabla de ejemplo</h1>
+    <Tabla :data="datos" textColor="white" backgroundColor="blue" />
+  </div>
+</template>
+
+<script>
+import Tabla from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Tabla
+  },
+  data() {
+    return {
+      datos: [
+        { nombre: 'Juan', edad: 25, email: 'juan@example.com' },
+        { nombre: 'María', edad: 30, email: 'maria@example.com' },
+        { nombre: 'Pedro', edad: 35, email: 'pedro@example.com' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  padding: 20px;
+}
+</
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <table :style="{ color: textColor, background: backgroundColor }">
+    <thead>
+      <tr>
+        <th>Nombre</th>
+        <th>Edad</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in data" :key="index">
+        <td>{{ item.nombre }}</td>
+        <td>{{ item.edad }}</td>
+        <td>{{ item.email }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      required: true
+    },
+    textColor: {
+      type: String,
+      default: 'black'
+    },
+    backgroundColor: {
+      type: String,
+      default: 'white'
+    }
+  }
+}
+</script>
+
+<style scoped>
+table {
+  
+  width: 0;
+  border-collapse: collapse;
+}
+
+th, td {
+  padding: 8px;
+  border-bottom: 1px solid #ddd;
+}
+
+th {
+  text-align: left;
+}
+
+tr:nth-child(even) {
+  background-color: #4f56d4;
+}
+</style>
